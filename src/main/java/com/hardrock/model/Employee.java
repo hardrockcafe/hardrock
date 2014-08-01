6df37f145a7fe6c69521d73766a8050aba97300d
srinivasa <a_srinivas54@reddifmail.com>
2014-08-01 08:02:03 +0530

    Added employee model class.

---
+++ src/main/java/com/hardrock/model/Employee.java
@@ -0,0 +1,55 @@
+package com.hardrock.model;
+
+public class Employee {
+
+	private int empId;
+
+	private String empName;
+
+	private String empDes;
+
+	/**
+	 * @return the empId
+	 */
+	public int getEmpId() {
+		return empId;
+	}
+
+	/**
+	 * @param empId
+	 *            the empId to set
+	 */
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	/**
+	 * @return the empName
+	 */
+	public String getEmpName() {
+		return empName;
+	}
+
+	/**
+	 * @param empName
+	 *            the empName to set
+	 */
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	/**
+	 * @return the empDes
+	 */
+	public String getEmpDes() {
+		return empDes;
+	}
+
+	/**
+	 * @param empDes
+	 *            the empDes to set
+	 */
+	public void setEmpDes(String empDes) {
+		this.empDes = empDes;
+	}
+}
